a5c8d7d8e100e1d6d3663c29a23bc07ad5d8b90d
lemikhovalex <lemikhovalex@gmail.com>
2019-05-07 23:23:33 +0300

    vrode vse

---
+++ Lemikhov_F_Yandex.swift
@@ -0,0 +1,23 @@
+var N_s = readLine()
+var N_wr = Int(String(N_s ?? ""))
+var N = N_wr!
+var array_sub: [String.SubSequence] = []
+var array: [Int] = []
+var Global_array: [Int] = []
+for i in 0...N-1
+{
+	array_sub = readLine()!.split(separator: " ")
+	for j in 0...array_sub.count-1
+	{
+		array.append( Int(String (array_sub[j]) )! )
+	}
+	
+	for j in 0...array.count-1
+	{
+		Global_array.append(array[j])
+	}
+	array.removeAll()
+}
+Global_array.sort()
+print(Global_array)
+
